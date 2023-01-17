5429f9f667de3aa65a17ce1e905c654127d662ba
Wenwei Lin <wenwei.lin.me@gmail.com>
2023-01-17 20:50:59 +0800

    Build notion.py module

---
+++ persistence/notion.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+
+class NotionManager:
+    def __init__(self, token):
+        self.header = {
+            "Accept": "application/json",
+            "Notion-Version": "2022-06-28",
+            "content-type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+
+    def create(self, payload: dict):
+        endpoint = "https://api.notion.com/v1/pages"
+        response = requests.post(
+            endpoint, json.dumps(payload), headers=self.header
+        ).json()
+        # TODO: Handle response 400
+        return response
+
+    def update(self, page_id, payload):
+        endpoint = f"https://api.notion.com/v1/pages/{page_id}"
+        response = requests.patch(
+            endpoint, json.dumps(payload), headers=self.header
+        ).json()
+        return response
+
+    def delete(self, page_id):
+        endpoint = f"https://api.notion.com/v1/pages/{page_id}"
+        payload = {"archived": True}
+        response = requests.patch(endpoint, json.dumps(payload), headers=self.header)
+        return response
+
+    def query(self, database_id, payload):
+        endpoint = f"https://api.notion.com/v1/databases/{database_id}/query"
+        response = requests.post(endpoint, json.dumps(payload), headers=self.header)
+        return response
